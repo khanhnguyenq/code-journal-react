ff0a49c787be162f95307205121cbde440b10509
Hagop Almadjian <hagop1isaboss@hotmail.com>
2024-01-08 18:36:01 +0000

    Commited my app

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+import './reset.css';
+import './styles.css';
+import './App.css';
+import { NavBar } from './NavBarComponent';
+
+function App() {
+  return <NavBar />;
+}
+
+export default App;
